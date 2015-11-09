0c49db6a34c9dfc5cae07b1ec9f0c6bffeecd4eb
John Sanda <jsanda@redhat.com>
2015-11-09 11:18:46 -0500

    update end-to-end test to verify that alert is fired

--- dist-itests/src/test/groovy/DistITest.groovy
+++ dist-itests/src/test/groovy/DistITest.groovy
@@ -1,5 +1,9 @@
+import com.google.common.base.Charsets
 import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
+import org.hawkular.alerts.api.model.condition.ThresholdCondition
+import org.hawkular.alerts.api.model.trigger.Trigger
+import org.junit.BeforeClass
 import org.junit.Test
 
 import javax.jms.*
@@ -32,15 +36,63 @@ import static org.junit.Assert.assertTrue
 class DistITest {
 
   static String tenantHeaderName = "Hawkular-Tenant"
-  static RESTClient hawkularMetrics
+  static RESTClient metricsClient
+  static RESTClient alertsClient
+
+  static String tenant = '28026b36-8fe4-4332-84c8-524e173a68bf'
+
+  @BeforeClass
+  static void initClients() {
+    String baseMetricsURI = System.getProperty('hawkular-metrics.base-uri') ?: '127.0.0.1:8080/hawkular/metrics'
+    String baseAlertsURI = System.getProperty('hawkular-alerts.base-uri') ?: '127.0.0.1:8080/hawkular/alerts'
+
+    def failureHandler = { resp ->
+      def msg = "Got error response: ${resp.statusLine}"
+      if (resp.entity != null && resp.entity.contentLength != 0) {
+        def baos = new ByteArrayOutputStream()
+        resp.entity.writeTo(baos)
+        def entity = new String(baos.toByteArray(), Charsets.UTF_8)
+        msg = """${msg}
+=== Response body
+${entity}
+===
+"""
+      }
+      System.err.println(msg)
+      return resp
+    }
+
+    metricsClient = new RESTClient("http://$baseMetricsURI/", ContentType.JSON)
+    metricsClient.handler.failure = failureHandler
+
+    alertsClient = new RESTClient("http://$baseAlertsURI/", ContentType.JSON)
+    alertsClient.handler.failure = failureHandler
+    alertsClient.headers.put("Hawkular-Tenant", tenant)
+  }
 
   @Test
-  void listenToTopic() {
-    String baseURI = System.getProperty('hawkular-metrics.base-uri') ?: '127.0.0.1:8080/hawkular/metrics'
-    hawkularMetrics = new RESTClient("http://$baseURI/", ContentType.JSON)
+  void endToEndTest() {
+    String metric = 'G1'
+
+    waitForAlertsToInitialize()
+
+    Trigger trigger = new Trigger("test-trigger-1", metric)
+    trigger.enabled = true
+    trigger.tags.metric = metric
+    def resp = alertsClient.delete(path: "triggers/$trigger.id")
+    assert(200 == resp.status || 404 == resp.status)
+
+    resp = alertsClient.post(path: "triggers", body: trigger)
+    assertEquals(200, resp.status)
+
+    resp = alertsClient.put(path: "triggers/$trigger.id/conditions/firing", body: [
+        new ThresholdCondition(trigger.id, trigger.name, ThresholdCondition.Operator.GT, 10.12)
+    ])
+    assertEquals(200, resp.status)
+    assertEquals(1, resp.data.size())
 
     Properties env = new Properties();
-    env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory")
     env.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8083")
     env.put(Context.SECURITY_PRINCIPAL, 'admin')
     env.put(Context.SECURITY_CREDENTIALS, 'redhat')
@@ -58,16 +110,34 @@ class DistITest {
       latch.countDown()
     }
 
-    String tenantId = 'TEST_TENANT'
-    def response = hawkularMetrics.post(path: "gauges/G1/data", body: [
+    def response = metricsClient.post(path: "gauges/$metric/data", body: [
         [timestamp: System.currentTimeMillis(), value: 12.22],
-        [timestamp: System.currentTimeMillis() - 500, value: 15.37],
-    ], headers: [(tenantHeaderName): tenantId])
+        [timestamp: System.currentTimeMillis() - 500, value: 8.4],
+    ], headers: [(tenantHeaderName): tenant])
     assertEquals(200, response.status)
 
     latch.await(5, TimeUnit.SECONDS)
 
     assertTrue(messageReceived)
+
+    // Wait a little bit to allow time for the trigger to fire an alert
+    Thread.sleep(3000)
+
+    response = alertsClient.get(path: '', query: [tags:"metric|$metric",thin:true])
+    assertEquals(200, response.status)
+
+    assertEquals(1, response.data.size())
+  }
+
+  static void waitForAlertsToInitialize() {
+    def response = alertsClient.get(path: 'plugins')
+    int attempts = 0
+    while (response.status != 200 && attempts++ < 20) {
+      Thread.sleep(1000)
+      response = alertsClient.get(path: 'plugins')
+    }
+
+    assertEquals('Failed to verify that Hawkular Alerts is initialized', 200, response.status)
   }
 
 }
