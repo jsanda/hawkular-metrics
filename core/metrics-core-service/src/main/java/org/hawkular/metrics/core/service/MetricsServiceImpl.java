8dcadef2ab92c6a74d26224e7294c7c53e2aaebe
John Sanda <jsanda@redhat.com>
2017-06-02 19:13:42 -0400

    remove debug logging

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
@@ -838,11 +838,7 @@ public class MetricsServiceImpl implements MetricsService {
         if (!stacked) {
             if (!isRate) {
                 return Observable.from(metrics)
-                        .flatMap(metricId -> {
-                            log.infof("Fetching data points for %s", metricId);
-                            return findDataPoints(metricId, start, end, 0, Order.DESC)
-                                    .doOnCompleted(() -> log.infof("Finished fetching data points for %s", metricId));
-                        })
+                        .flatMap(metricId -> findDataPoints(metricId, start, end, 0, Order.DESC))
                         .compose(new NumericBucketPointTransformer(buckets, percentiles));
             } else {
                 return Observable.from(metrics)
